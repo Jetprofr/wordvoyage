8585b23a2266bfef7d302a2eb38f9d07b34b2d6e
Jetprofr <karim.bendahou@gmail.com>
2025-07-03 15:34:00 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,11 +2,16 @@
 self.addEventListener('install', event => {
   console.log('[ServiceWorker] Install');
   event.waitUntil(
-    caches.open('wordvoyage-cache-v1').then(cache => {
+    caches.open('wordvoyage-cache-v2').then(cache => {
       return cache.addAll([
         './',
         './index.html',
-        './icon_thai.png'
+        './manifest.json',
+        './splash_screen.png',
+        './css/style.css',
+        './js/script.js',
+        './images/flags/thai.png',
+        './pages/thai/index_thai.html'
       ]);
     })
   );
